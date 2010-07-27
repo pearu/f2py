2946787f6a340f596fa13d99fa7d3e480b2b02c7
pearu.peterson <pearu.peterson@localhost>
2010-07-27 11:00:52 +0000

    Added hg revision number support to version computation.

--- f2py/version.py
+++ f2py/version.py
@@ -1,12 +1,16 @@
 
 # THIS FILE IS GENERATED FROM F2PY SETUP.PY
-short_version='0.3.0'
-version='0.3.0'
+short_version='0.3.1'
+version='0.3.1'
 release=False
 
 if not release:
     version += '.dev'
     import os
+    hg_version_file = os.path.join(os.path.dirname(__file__),
+                                   '__hg_version__.py')
+    hg_branch_file = os.path.join(os.path.dirname(__file__),'.hg',
+                                   'branch')
     svn_version_file = os.path.join(os.path.dirname(__file__),
                                    '__svn_version__.py')
     svn_entries_file = os.path.join(os.path.dirname(__file__),'.svn',
@@ -18,6 +22,13 @@ if not release:
                               svn_version_file,
                               ('.py','U',1))
         version += svn.version
+    elif os.path.isfile(hg_version_file):
+        import imp
+        hg = imp.load_module('f2py.__hg_version__',
+                              open(hg_version_file),
+                              hg_version_file,
+                              ('.py','U',1))
+        version += hg.version
     elif os.path.isfile(svn_entries_file):
         import subprocess
         try:
@@ -26,5 +37,13 @@ if not release:
             pass
         else:
             version += svn_version.strip()
+    elif os.path.isfile(hg_branch_file):
+        import subprocess
+        try:
+            hg_version = subprocess.Popen(["hg identify --num", os.path.dirname (__file__)], stdout=subprocess.PIPE).communicate()[0]
+        except:
+            pass
+        else:
+            version += hg_version.strip()
 
 print version
--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ Operating System :: MacOS
 PLATFORMS = ['Linux', 'Windows', 'MacOS']
 MAJOR               = 0
 MINOR               = 3
-MICRO               = 0
+MICRO               = 1
 ISRELEASED          = not True
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
@@ -69,6 +69,10 @@ release=%(isrelease)s
 if not release:
     version += '.dev'
     import os
+    hg_version_file = os.path.join(os.path.dirname(__file__),
+                                   '__hg_version__.py')
+    hg_branch_file = os.path.join(os.path.dirname(__file__),'.hg',
+                                   'branch')
     svn_version_file = os.path.join(os.path.dirname(__file__),
                                    '__svn_version__.py')
     svn_entries_file = os.path.join(os.path.dirname(__file__),'.svn',
@@ -80,6 +84,13 @@ if not release:
                               svn_version_file,
                               ('.py','U',1))
         version += svn.version
+    elif os.path.isfile(hg_version_file):
+        import imp
+        hg = imp.load_module('f2py.__hg_version__',
+                              open(hg_version_file),
+                              hg_version_file,
+                              ('.py','U',1))
+        version += hg.version
     elif os.path.isfile(svn_entries_file):
         import subprocess
         try:
@@ -88,6 +99,14 @@ if not release:
             pass
         else:
             version += svn_version.strip()
+    elif os.path.isfile(hg_branch_file):
+        import subprocess
+        try:
+            hg_version = subprocess.Popen(["hg identify --num", os.path.dirname (__file__)], stdout=subprocess.PIPE).communicate()[0]
+        except:
+            pass
+        else:
+            version += hg_version.strip()
 
 print version
 """
