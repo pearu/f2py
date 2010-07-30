d0df78e9f660b138b5f79d6714312740ebcf1648
Pearu Peterson <pearu.peterson@gmail.com>
2010-07-30 22:31:02 +0300

    Add log.config to data files to fix installed fparser.

--- fparser/setup.py
+++ fparser/setup.py
@@ -2,4 +2,5 @@
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('fparser',parent_package,top_path)
+    config.add_data_files('log.config')
     return config
