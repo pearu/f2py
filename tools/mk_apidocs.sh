4992120ad005861f828f48981d4b932bb954bb7d
Pearu Peterson <pearu.peterson@gmail.com>
2010-07-30 13:31:15 +0300

    Added apidocs scripts.

--- tools/mk_apidocs.sh
+++ tools/mk_apidocs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+test -d doc || (echo "Run me from f2py directory as tools/mk_apidocs.sh" && exit 1) || exit 1
 HTML_TARGET=html
 rm -rf doc/_build/ doc/source/generated $HTML_TARGET/*
 echo `date -u` ": documentation is being updated... try again in few minutes." >  $HTML_TARGET/index.html
