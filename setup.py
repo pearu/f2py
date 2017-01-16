8dd30e3861b33e8d52a4fb40ec74d83445bc6410
Andrew Porter <andrew.porter@stfc.ac.uk>
2017-01-16 11:22:52 +0000

    #1 2nd attempt to simplify setup.py and correct MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,199 +1,82 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2017, Andrew Porter.
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""Setup script. Used by easy_install and pip."""
+
+from setuptools import setup, find_packages
 import os
-import sys
-from os.path import join, basename, dirname, splitext
-from glob import glob
 
-from numpy.distutils import log
-from distutils.dep_util import newer
-
+PACKAGES = find_packages(where="src")
 
 NAME = 'f2py'
-AUTHOR = 'Pearu Peterson'
-AUTHOR_EMAIL = 'pearu.peterson@gmail.com'
+AUTHOR = 'Andrew Porter'
+AUTHOR_EMAIL = 'trackstand.andy@gmail.com'
 LICENSE = 'BSD'
-URL = 'http://code.google.com/p/f2py/'
-DOWNLOAD_URL = 'http://code.google.com/p/f2py/downloads/list'
-DESCRIPTION = 'The F2PY Project'
+URL = 'https://github.com/stfc/fparser'
+DOWNLOAD_URL = 'https://github.com/stfc/fparser'
+DESCRIPTION = 'The fparser Project'
 LONG_DESCRIPTION = '''\
-The F2PY project is created to unify the efforts of supporting easy
-connection between Fortran and Python languages. The project will
-provide software, documentation, and support to solve a difficult task
-of automatically generating Python wrappers to Fortran libraries.
+The fparser project is created to develop a parser for
+Fortran 77..2003 code. It is based on the work of Pearu Peterson in
+the F2PY project (http://www.f2py.com). 
 
-The following packages are provided:
-
-  fparser - Fortran 66/77/90/95/2003 parser
-
-The aim is to have a tool, called f2py, that can automatically
-generate Python wrappers to any Fortran program, be it written in
-Fortran 66/77 or in Fortran 90/95/2003.
-
-See http://code.google.com/p/f2py/ for more information.
+See https://github.com/stfc/fparser for more information.
 '''
 CLASSIFIERS = """\
 Development Status :: 3 - Alpha
+Environment :: Console
 Intended Audience :: Developers
 Intended Audience :: Science/Research
 License :: OSI Approved
 Natural Language :: English
-Programming Language :: C
 Programming Language :: Fortran
 Programming Language :: Python
 Topic :: Scientific/Engineering
 Topic :: Software Development
-Topic :: Software Development :: Code Generators
-Operating System :: Microsoft :: Windows
+Topic :: Utilities,
 Operating System :: POSIX
 Operating System :: Unix
 Operating System :: MacOS
 """
 
-PLATFORMS = ['Linux', 'Windows', 'MacOS']
+PLATFORMS = ['Linux', 'MacOS']
 MAJOR               = 0
-MINOR               = 3
+MINOR               = 0
 MICRO               = 1
 ISRELEASED          = not True
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
-if os.path.exists('MANIFEST'): os.remove('MANIFEST')
-
-def write_version_py(filename='f2py/version.py'):
-    cnt = """
-# THIS FILE IS GENERATED FROM F2PY SETUP.PY
-short_version='%(version)s'
-version='%(version)s'
-release=%(isrelease)s
-
-if not release:
-    version += '.dev'
-    import os
-    hg_version_file = os.path.join(os.path.dirname(__file__),
-                                   '__hg_version__.py')
-    hg_branch_file = os.path.join(os.path.dirname(__file__),'.hg',
-                                   'branch')
-    svn_version_file = os.path.join(os.path.dirname(__file__),
-                                   '__svn_version__.py')
-    svn_entries_file = os.path.join(os.path.dirname(__file__),'.svn',
-                                   'entries')
-    if os.path.isfile(svn_version_file):
-        import imp
-        svn = imp.load_module('f2py.__svn_version__',
-                              open(svn_version_file),
-                              svn_version_file,
-                              ('.py','U',1))
-        version += svn.version
-    elif os.path.isfile(hg_version_file):
-        import imp
-        hg = imp.load_module('f2py.__hg_version__',
-                              open(hg_version_file),
-                              hg_version_file,
-                              ('.py','U',1))
-        version += hg.version
-    elif os.path.isfile(svn_entries_file):
-        import subprocess
-        try:
-            svn_version = subprocess.Popen(["svnversion", os.path.dirname (__file__)], stdout=subprocess.PIPE).communicate()[0]
-        except:
-            pass
-        else:
-            version += svn_version.strip()
-    elif os.path.isfile(hg_branch_file):
-        import subprocess
-        try:
-            hg_version = subprocess.Popen(["hg identify --num", os.path.dirname (__file__)], stdout=subprocess.PIPE).communicate()[0]
-        except:
-            pass
-        else:
-            version += hg_version.strip()
-
-print version
-"""
-    a = open(filename, 'w')
-    try:
-        a.write(cnt % {'version': VERSION, 'isrelease': str(ISRELEASED)})
-    finally:
-        a.close()
-
-
-def configuration(parent_package='',top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration(None,parent_package,top_path)
-    config.add_subpackage('f2py')
-    config.add_subpackage('fparser')
-    config.get_version('f2py/version.py')
-
-    scripts = []
-    scripts += glob(join(config.local_path, 'fparser','scripts', '*.py'))
-    scripts += glob(join(config.local_path, 'f2py','scripts', '*.py'))
-
-    wininst = 'bdist_wininst' in sys.argv
-
-    for script in scripts:
-
-        subpackage = basename(dirname(dirname(script)))
-        
-        if basename (script).startswith (subpackage):
-            config.add_scripts(script)
-            continue
-
-        def generate_a_script(build_dir, script=script, config=config):
-            dist = config.get_distribution()
-            install_lib = dist.get_command_obj('install_lib')
-            if not install_lib.finalized:
-                install_lib.finalize_options()
-
-            script_replace_text = ''
-            install_lib = install_lib.install_dir
-            if install_lib is not None:
-                script_replace_text = '''
-import sys
-if %(d)r not in sys.path:
-    sys.path.insert(0, %(d)r)
-''' % dict(d=install_lib)
-
-            start_mark = '### START UPDATE SYS.PATH ###'
-            end_mark = '### END UPDATE SYS.PATH ###'
-            name = basename(script)
-            if name.startswith (subpackage):
-                target_name = name
-            elif wininst:
-                target_name = subpackage + '_' + name
-            else:
-                target_name = subpackage + '.' + splitext(name)[0]
-            target = join(build_dir, target_name)
-            if newer(script, target) or 1:
-                log.info('Creating %r', target)
-                f = open (script, 'r')
-                text = f.read()
-                f.close()
-
-                i = text.find(start_mark)
-                if i != -1:
-                    j = text.find (end_mark)
-                    if j == -1:
-                        log.warn ("%r missing %r line", script, start_mark)
-                    new_text = text[:i+len (start_mark)] + script_replace_text + text[j:]
-                else:
-                    new_text = text
-
-                f = open(target, 'w')
-                f.write(new_text)
-                f.close()
-        config.add_scripts(generate_a_script)
-
-    return config
-
 if __name__ == '__main__':
-    from numpy.distutils.core import setup
-
-    # Rewrite the version file everytime
-    if os.path.exists('f2py/version.py'): os.remove('f2py/version.py')
-    write_version_py()
 
     setup(
         name = NAME,
+        version = VERSION,
         author = AUTHOR,
         author_email = AUTHOR_EMAIL,
         license = LICENSE,
@@ -202,5 +85,10 @@ if __name__ == '__main__':
         description = DESCRIPTION,
         long_description = LONG_DESCRIPTION,
         classifiers = filter(None, CLASSIFIERS.split('\n')),
+        packages=PACKAGES,
+        package_dir={"", "src"},
+        # We need the following line to ensure we get the fparser/log.config
+        # file installed.
+        include_package_data=True,
         platforms = PLATFORMS,
         configuration=configuration)
