89961871b6627f29a26b7077208b7c67c5ef8457
Andrew Porter <andrew.porter@stfc.ac.uk>
2017-06-12 17:52:07 +0100

    #2 Add functional test for subroutine containing problematic procedure declaration

---
+++ src/fparser/tests/test_functional.py
@@ -0,0 +1,40 @@
+def test_procedure_interface():
+    ''' Functional test that parser copes with a procedure declaration '''
+    from fparser import api
+    source_str = '''  subroutine divergence_diagnostic_alg(u, t, mesh_id)
+    use dg_matrix_vector_kernel_mod, only: dg_matrix_vector_kernel_type
+    use runtime_constants_mod,       only: get_mass_matrix, &
+                                           get_div, &
+                                           w3inv_id
+    use operator_mod,                only: operator_type
+    use field_mod,                   only: field_type, write_interface
+    use fs_continuity_mod,           only: W3
+    use psykal_lite_mod,             only: invoke_inner_prod
+    use log_mod,                     only: log_event,         &
+                                           log_scratch_space, &
+                                           LOG_LEVEL_INFO
+
+    implicit none 
+    type(field_type), intent(in) :: u
+    integer(i_def),   intent(in) :: t, mesh_id
+
+    type(field_type)             :: divergence, div_u
+    type(operator_type), pointer :: div => null(), m3_inv => null()
+    real(r_def)                  :: l2
+
+    !procedure (write_interface), pointer  :: tmp_ptr
+    procedure (a) :: tmp_ptr
+
+    divergence =  field_type( vector_space = function_space_collection%get_fs(mesh_id,element_order, W3) )
+    div_u      =  field_type( vector_space = function_space_collection%get_fs(mesh_id,element_order, W3) )
+    div    => get_div()
+    m3_inv => get_mass_matrix(w3inv_id)
+    call invoke( dg_matrix_vector_kernel_type( div_u, u, div) )
+
+  end subroutine divergence_diagnostic_alg
+'''
+    tree = api.parse(source_str, isfree=True, isstrict=False,
+                     ignore_comments=False)
+    print str(tree)
+    assert 0
+
