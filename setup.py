f6675bc933192c999c293c726869baa98db5a3d4
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2017-11-21 11:21:09 +0000

    There was an unspecified dependency on the "six" package which is now explicit.

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
         classifiers=CLASSIFIERS,
         packages=PACKAGES,
         package_dir={"": "src"},
-        install_requires=['nose'],
+        install_requires=['nose', 'six'],
         # We need the following line to ensure we get the fparser/log.config
         # file installed.
         include_package_data=True)
