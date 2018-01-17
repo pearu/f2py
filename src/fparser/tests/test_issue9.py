0e6f0eb13e76e3f231a4abc037f2b0b47e481834
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-01-17 17:23:11 +0000

    #68 fix test now we strip blank lines

--- src/fparser/tests/test_issue9.py
+++ src/fparser/tests/test_issue9.py
@@ -81,11 +81,8 @@ def test_reproduce_issue():
     assert str(tree).strip().split('\n')[1:]=='''
       !      BEGINSOURCE <cStringIO.StringI object at 0x2405ea0> mode=fix90
         MODULE m_rdctrl
-
           CONTAINS
-
           SUBROUTINE readctrl(prgn, vstrn, vn)
-
           END SUBROUTINE readctrl
         END MODULE m_rdctrl
     '''.strip().split('\n')[1:]
