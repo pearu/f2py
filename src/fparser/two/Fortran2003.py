0f32e9aa0888dc0300d0dc199141e31834efd7dc
pelson <pelson.pub@gmail.com>
2019-03-01 14:28:21 +0000

    Enforce strict ordering of main program.

--- src/fparser/two/Fortran2003.py
+++ src/fparser/two/Fortran2003.py
@@ -8384,7 +8384,7 @@ class Main_Program(BlockBase):  # R1101 [C1101, C1102, C1103]
         return BlockBase.match(
             Program_Stmt, [Specification_Part, Execution_Part,
                            Internal_Subprogram_Part], End_Program_Stmt,
-            reader, match_names=True)
+            reader, match_names=True, strict_order=True)
 
 
 class Main_Program0(BlockBase):
