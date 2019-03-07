bc31a82ac045903fa3ac651385d24e4a59413a43
Rupert Ford <rupert.ford@stfc.ac.uk>
2019-03-07 11:08:45 +0000

    pr #185. Revoke of a debug change.

--- src/fparser/scripts/fparser2.py
+++ src/fparser/scripts/fparser2.py
@@ -93,7 +93,7 @@ def runner(_, options, args):
         try:
             f2003_parser = ParserFactory().create()
             program = f2003_parser(reader)
-            print (repr(program))
+            print (program)
         except FortranSyntaxError as msg:
             print ("Syntax error: {0}".format(str(msg)))
             try:
