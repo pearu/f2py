1fa18432e9cc78bea27a1e7adecf37b30bc98052
Andrew Porter <andrew.porter@stfc.ac.uk>
2019-11-29 14:26:28 +0000

    #226 add tests showing problem [skip ci]

---
+++ src/fparser/two/tests/fortran2003/test_case_construct_r808.py
@@ -0,0 +1,37 @@
+# This Python file uses the following encoding: utf-8
+from fparser.common.readfortran import FortranFileReader, FortranStringReader
+from fparser.two.Fortran2003 import Program, Case_Construct
+import six
+
+def test_tofortran_non_ascii(f2003_create):
+    ''' Check that the tofortran() method works when the character string
+    contains non-ascii characters. '''
+    code = (u"SELECT CASE(iflag)\n"
+            u"CASE(  30  ) ! This is a comment\n"
+            u"  IF(lwp) WRITE(*,*) ' for e1=1\xb0'\n"
+            u"END SELECT\n")
+    reader = FortranStringReader(code, ignore_comments=False)
+    obj = Case_Construct(reader)
+    out_str = obj.tofortran()
+    assert "for e1=1" in out_str
+    print(out_str)
+    assert 0
+
+
+def test_blockbase_tofortran_non_ascii(f2003_create):
+    ''' Check that the tofortran() method works when the character string
+    contains non-ascii characters. and there's a comment outside the select
+    block. '''
+    code = (u"program my_test\n"
+            u"! A comment outside the select block\n"
+            u"SELECT CASE(iflag)\n"
+            u"CASE(  30  )\n"
+            u"  IF(lwp) WRITE(*,*) ' for e1=1\xb0'\n"
+            u"END SELECT\n"
+            u"end program\n")
+    reader = FortranStringReader(code, ignore_comments=False)
+    obj = Program(reader)
+    out_str = obj.tofortran()
+    assert "for e1=1" in out_str
+    print(out_str)
+    assert 0
