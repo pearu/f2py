5e39a4b1d50db1016289922ad3fa8f196a14b93d
Andrew Porter <andrew.porter@stfc.ac.uk>
2020-01-09 16:09:37 +0000

    #230 mv test for get_child into new file

--- src/fparser/two/tests/test_utils.py
+++ src/fparser/two/tests/test_utils.py
@@ -110,31 +110,3 @@ def test_blockbase_match_name_classes(f2003_create):
         ast = If_Construct(reader)
     assert ("at line 2\n>>>endif label\nName 'label' has no corresponding "
             "starting name") in str(excinfo.value)
-
-
-@pytest.mark.usefixtures("f2003_create")
-def test_get_child():
-    ''' Test the get_child() utility. '''
-    from fparser.two import Fortran2003
-    from fparser.two.utils import walk, get_child
-    reader = get_reader("program hello\n"
-                        "write(*,*) 'hello'\n"
-                        "write(*,*) 'goodbye'\n"
-                        "end program hello\n")
-    main = Fortran2003.Program(reader)
-    prog = get_child(main, Fortran2003.Main_Program)
-    exe = get_child(prog, Fortran2003.Execution_Part)
-    assert isinstance(exe, Fortran2003.Execution_Part)
-    write_stmt = get_child(exe, Fortran2003.Write_Stmt)
-    # Check that we got the first write and not the second
-    assert "goodbye" not in str(write_stmt)
-    # The top level has no Io_Control_Spec children
-    assert not get_child(main, Fortran2003.Io_Control_Spec)
-    # Check functionality when node has children in `items` and
-    # not in `content`
-    io_nodes = walk(main.content, Fortran2003.Io_Control_Spec)
-    assert not hasattr(io_nodes[0], "content")
-    io_unit = get_child(io_nodes[0], Fortran2003.Io_Unit)
-    assert isinstance(io_unit, Fortran2003.Io_Unit)
-    missing = get_child(io_nodes[0], Fortran2003.Execution_Part)
-    assert missing is None
