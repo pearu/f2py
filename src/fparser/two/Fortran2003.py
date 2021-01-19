b526ba0c079b6669557f71200acfdf0cb1295420
rupertford <rupert.ford@stfc.ac.uk>
2021-01-19 01:51:18 +0000

    issue #288. Reverting accidental changes on master.

--- src/fparser/two/Fortran2003.py
+++ src/fparser/two/Fortran2003.py
@@ -4730,39 +4730,19 @@ class Mult_Operand(BinaryOpBase):  # R704
     match = staticmethod(match)
 
 
-class Add_Operand(BinaryOpBase):  # pylint: disable=invalid-name
-    '''Fortran 2003 rule R705
-
-    add-operand is [ add-operand mult-op ] mult-operand
-
-    Rule R705 in implemented in two parts, the first with the optional
-    part included (in the match method for this class) and the second
-    without the optional part (specified in subclass_names).
-
-    Note rule R708 (mult-op is * or /) is implemented directly here as
-    the mult_op pattern.
-
-    Rule R705 specifies matching using 'add-operand', however this
-    implementation uses Level_2_Expr instead. The reason for this is
-    due to the potential to accidentally match a negative exponent as
-    the minus sign in a level-2-expr. If this happens then it is
-    possible to end up matching a * or / (a level 1 expression) before
-    matching a valid + or - which would normally result in no match
-    overall as * or / are matched after + or -. By matching with
-    Level_2_Expr, this allows us to match with a * or / and then a +
-    or - afterwards. A particular example is "a + 1.0e-1 * c", where
-    (rightly) failing to match on the "-" leads us to try to match on
-    the "*" which then fails to match on the + (as + and - have
-    already been tested).
-
-    '''
+class Add_Operand(BinaryOpBase):  # R705
+    """
+    <add-operand> = [ <add-operand> <mult-op> ] <mult-operand>
+    <mult-op>  = *
+                 | /
+    """
     subclass_names = ['Mult_Operand']
-    use_names = ['Level_2_Expr', 'Mult_Operand']
+    use_names = ['Add_Operand', 'Mult_Operand']
 
-    @staticmethod
     def match(string):
         return BinaryOpBase.match(
-            Level_2_Expr, pattern.mult_op.named(), Mult_Operand, string)
+            Add_Operand, pattern.mult_op.named(), Mult_Operand, string)
+    match = staticmethod(match)
 
 
 class Level_2_Expr(BinaryOpBase):  # R706
@@ -4795,9 +4775,9 @@ class Level_2_Unary_Expr(UnaryOpBase):  # R706.c
             pattern.add_op.named(), Add_Operand, string)
     match = staticmethod(match)
 
-# R707: power-op is **
-# R708: mult-op is * or /
-# R709: add-op is + or -
+# R707: <power-op> = **
+# R708: <mult-op> = * | /
+# R709: <add-op> = + | -
 
 
 class Level_3_Expr(BinaryOpBase):  # R710
