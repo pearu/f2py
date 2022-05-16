dcff588bf13d2d86244a151ad3697b53ae232a93
Peter Hill <peter.hill@york.ac.uk>
2022-05-16 16:06:13 +0100

    Fix path for setuptools_scm

--- src/fparser/__init__.py
+++ src/fparser/__init__.py
@@ -75,7 +75,7 @@ try:
 except PackageNotFoundError:
     from setuptools_scm import get_version
 
-    __version__ = get_version(root="..", relative_to=__file__)
+    __version__ = get_version(root="../..", relative_to=__file__)
 
 import logging
 import codecs
