bb60bedfe0613475d319f51642e508e02af8d03c
Andrew Porter <andrew.porter@stfc.ac.uk>
2022-06-09 11:43:39 +0100

    #329 tidying for pylint

--- src/fparser/scripts/fparser2_bench.py
+++ src/fparser/scripts/fparser2_bench.py
@@ -39,75 +39,92 @@ it takes fparser2 to parse it. This is based on the benchmark suggested
 by Ondřej Čertík via Ioannis Nikiteas.
 
 '''
+from time import perf_counter
+
 from fparser.common.sourceinfo import FortranFormat
 from fparser.common.readfortran import FortranStringReader
 from fparser.two.parser import ParserFactory
-from time import perf_counter
 
 
-def gen_sub(no: int):
+def gen_sub(num: int):
     '''
-    Constructs a Fortran subroutine named g<no>.
+    Constructs a Fortran subroutine named g<num>.
 
-    :param no: the number of the subroutine (used to name it).
+    :param num: the number of the subroutine (used to name it).
 
     :returns: Fortran subroutine.
     :rtype: str
 
     '''
-    sub = """subroutine g{0}(x)
+    sub = f"""subroutine g{num}(x)
     integer, intent(inout) :: x
     integer :: i
     x = 0
-    do i = {0}, {1}
+    do i = {num}, {num+9}
         x = x+i
     end do
 end subroutine
 
-""".format(
-        no, no + 9
-    )
+"""
     return sub
 
 
-def create_bench():
+def create_bench(num_routines: int):
     '''
+    Creates the Fortran benchmark code.
+
+    :param num_routines: the number of subroutines to create.
+
     :returns: benchmark Fortran code.
     :rtype: str
-    '''
-    # Number of subroutines to create.
-    num_subroutines = 10000
 
+    '''
     code = ["program bench3",
             "implicit none",
             "integer :: c",
             "c = 0"]
-    for i in range(1, num_subroutines+1):
+    for i in range(1, num_routines+1):
         code.append(f"call g{i}(c)")
 
     code.append("print *, c")
     code.append("contains")
 
-    for i in range(1, num_subroutines+1):
+    for i in range(1, num_routines+1):
         code.append(gen_sub(i))
     code.append("end program")
 
     return "\n".join(code)
 
 
-if __name__ == "__main__":
+def runner(num_routines: int):
+    '''
+    Entry point for running the benchmark.
 
-    print("Constructing benchmark code...")
-    code = create_bench()
+    :param num_routines: the number of subroutines to create in the \
+                         Fortran benchmark.
+
+    :raises ValueError: if num_routines < 1.
+
+    '''
+    if num_routines < 1:
+        raise ValueError(f"Number of routines to create must be a positive, "
+                         f"non-zero integer but got: {num_routines}")
+
+    print(f"Constructing benchmark code with {num_routines} subroutines...")
+    code = create_bench(num_routines)
     reader = FortranStringReader(code)
     # Ensure the reader uses free format.
     reader.set_format(FortranFormat(True, True))
-    
+
     fparser = ParserFactory().create()
 
     print("Parsing benchmark code...")
     tstart = perf_counter()
-    program = fparser(reader)
+    _ = fparser(reader)
     tstop = perf_counter()
 
     print(f"Time taken for parse = {tstop - tstart:.2f}s")
+
+
+if __name__ == "__main__":
+    runner(10000)
