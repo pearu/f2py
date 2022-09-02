c1cb41c6bdc34a887799ee147163de4d7bf2ba2b
Andrew Porter <andrew.porter@stfc.ac.uk>
2022-09-02 10:43:48 +0100

    Update copyright date

--- src/fparser/two/tests/fortran2008/test_submodule_stmt_r1117.py
+++ src/fparser/two/tests/fortran2008/test_submodule_stmt_r1117.py
@@ -1,25 +1,25 @@
-# Copyright (c) 2018 Science and Technology Facilities Council
-
+# Copyright (c) 2018-2022 Science and Technology Facilities Council.
+#
 # All rights reserved.
-
+#
 # Modifications made as part of the fparser project are distributed
 # under the following license:
-
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-
+#
 # 1. Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
-
+#
 # 2. Redistributions in binary form must reproduce the above copyright
 # notice, this list of conditions and the following disclaimer in the
 # documentation and/or other materials provided with the distribution.
-
+#
 # 3. Neither the name of the copyright holder nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
-
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
