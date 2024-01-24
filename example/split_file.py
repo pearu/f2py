165999138408f2ba31d21afa217cedb1f434ed8a
Joerg Henrichs <henrichsjoerg@gmail.com>
2024-01-24 11:54:02 +1100

    #440 Refactored things to get rid of stupid pylint warnings.

--- example/split_file.py
+++ example/split_file.py
@@ -63,52 +63,18 @@ def usage():
 
 
 # -----------------------------------------------------------------------------
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        usage()
+def create_makefile(main_name, all_objs, all_filenames):
+    '''This function creates a Makefile to compile (and if a main_name is
+    given to link) the binaries. It uses the create_dependencies.py script
+    to add the actual dependencies between the modules to the makefile.
 
-    try:
-        reader = FortranFileReader(sys.argv[1])
-    except IOError:
-        print(f"Could not open file '{sys.argv[1]}'.", file=sys.stderr)
-        sys.exit(-1)
+    :param str main_name: name of the program, or "" if no program was
+        contained in the original file.
+    :param List[str] all_objs: the list of all objects to compile.
+    :param List[str] all_filenames: the list of all file names, used to
+        call the `create_dependency.py` script
 
-    Parser = ParserFactory().create(std="f2008")
-    parse_tree = Parser(reader)
-
-    # Get a list of all preprocessor classes (which will determine
-    # the extension to be used: F90/f90
-    ALL_CPP_CLASSES = tuple(getattr(C99Preprocessor, i)
-                            for i in C99Preprocessor.CPP_CLASS_NAMES)
-    all_filenames = []
-    all_objs = []
-    # pylint considers this a constant??
-    # pylint: disable=invalid-name
-    main_name = ""
-    # pylint: enable=invalid-name
-    for unit in parse_tree.children:
-        # For Module, Subroutine_Subprogram, Function_Subprogram, Main_Program
-        # we always get the name using this:
-        unit_name = unit.children[0].items[1].string.lower()
-
-        # Save the name of the main program if we have one, which will be
-        # used in the Makefile created later
-        if isinstance(unit, Fortran2003.Main_Program):
-            main_name = unit_name
-        if any(walk(unit, ALL_CPP_CLASSES)):
-            ext = ".F90"
-        else:
-            ext = ".f90"
-
-        filename = f"{unit_name}{ext}"
-        if os.path.exists(filename):
-            print(f"The file '{filename}' already exists - aborting.")
-            sys.exit(-1)
-        with open(filename, mode='w', encoding='utf-8') as f_out:
-            f_out.write(str(unit))
-        all_filenames.append(filename)
-        all_objs.append(f"{unit_name}.o")
-
+    '''
     file_path = os.path.dirname(os.path.realpath(__file__))
     completed = subprocess.run(
         [f"{file_path}/create_dependencies.py"]+all_filenames,
@@ -120,9 +86,9 @@ if __name__ == "__main__":
     f90flags = os.getenv("F90FLAGS", "-g -O0")
     ldflags = os.getenv("LDFLAGS", "")
 
-    MAKEFILE = "Makefile"
-    if os.path.exists(MAKEFILE):
-        print(f"The file '{MAKEFILE}' already exists - aborting.")
+    makefile = "Makefile"
+    if os.path.exists(makefile):
+        print(f"The file '{makefile}' already exists - aborting.")
         sys.exit(-1)
 
     # Now create a makefile
@@ -136,7 +102,7 @@ if __name__ == "__main__":
         default_target = "default: $(OBJS)"
         clean_actions = "\trm -f $(OBJS) *.mod"
 
-    with open(MAKEFILE, mode='w', encoding='utf-8') as f_out:
+    with open(makefile, mode='w', encoding='utf-8') as f_out:
         f_out.write(f"""
 F90 ?= {f90}
 # We have to enforce this setting, since using ?= will not
@@ -172,3 +138,61 @@ OBJS={' '.join(all_objs)}
 clean:
 {clean_actions}
 """)
+
+
+# -----------------------------------------------------------------------------
+def main():
+    '''The main program. Having this as a dedicated function solves many
+    pylint complains.
+
+    '''
+    if len(sys.argv) != 2:
+        usage()
+
+    try:
+        reader = FortranFileReader(sys.argv[1])
+    except IOError:
+        print(f"Could not open file '{sys.argv[1]}'.", file=sys.stderr)
+        sys.exit(-1)
+
+    Parser = ParserFactory().create(std="f2008")
+    parse_tree = Parser(reader)
+
+    # Get a list of all preprocessor classes (which will determine
+    # the extension to be used: F90/f90
+    all_cpp_classes = tuple(getattr(C99Preprocessor, i)
+                            for i in C99Preprocessor.CPP_CLASS_NAMES)
+    all_filenames = []
+    all_objs = []
+    main_name = ""
+    for unit in parse_tree.children:
+        # For Module, Subroutine_Subprogram, Function_Subprogram, Main_Program
+        # we always get the name using this:
+        unit_name = unit.children[0].items[1].string.lower()
+
+        # Save the name of the main program if we have one, which will be
+        # used in the Makefile created later
+        if isinstance(unit, Fortran2003.Main_Program):
+            main_name = unit_name
+        if any(walk(unit, all_cpp_classes)):
+            ext = ".F90"
+        else:
+            ext = ".f90"
+
+        filename = f"{unit_name}{ext}"
+        if os.path.exists(filename):
+            print(f"The file '{filename}' already exists - aborting.")
+            sys.exit(-1)
+        with open(filename, mode='w', encoding='utf-8') as f_out:
+            f_out.write(str(unit))
+        all_filenames.append(filename)
+        all_objs.append(f"{unit_name}.o")
+
+    # Now create the makefile
+    create_makefile(main_name, all_objs, all_filenames)
+
+
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+    # Using a function here avoids pylint warnings
+    main()
